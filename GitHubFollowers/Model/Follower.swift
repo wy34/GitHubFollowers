07ef456835cae5713f99e3a7a0cc00a83ca42a57
William Yeung <williamyeung1141998@gmail.com>
2021-03-21 00:51:47 -0400

    Created user and followerdata model

---
+++ GitHubFollowers/Model/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  GitHubFollowers
+//
+//  Created by William Yeung on 3/21/21.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
---
+++ GitHubFollowers/Model/User.swift
@@ -0,0 +1,22 @@
+//
+//  User.swift
+//  GitHubFollowers
+//
+//  Created by William Yeung on 3/21/21.
+//
+
+import Foundation
+
+struct User: Codable {
+    var login: String
+    var avatarUrl: String
+    var name: String?
+    var location: String?
+    var bio: String?
+    var publicRepos: Int
+    var publicGists: Int
+    var htmlUrl: String
+    var following: Int
+    var followers: Int
+    var createdAt: String
+}
