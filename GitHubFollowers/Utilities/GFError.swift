7d0e57a0c64c2437b4f54bf1b5c15c4de00b3b42
William Yeung <williamyeung1141998@gmail.com>
2021-03-21 23:16:42 -0400

    Refactored getFollowers method to use Result type

---
+++ GitHubFollowers/Utilities/GFError.swift
@@ -0,0 +1,15 @@
+//
+//  ErrorMessage.swift
+//  GitHubFollowers
+//
+//  Created by William Yeung on 3/21/21.
+//
+
+import Foundation
+
+enum GFError: String, Error {
+    case invalidUsername = "This username created an invalid request. Please try again."
+    case unableToComplete = "Unable to complete your request. Please check your internet connection"
+    case invalidResponse = "Invalid response from the server. Please try again."
+    case invalidData = "The data received from the server was invalid. Please try again."
+}
