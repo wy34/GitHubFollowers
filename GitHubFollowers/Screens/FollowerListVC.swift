6afabd4d87079d072dc2038e577ba1a0185843aa
William Yeung <williamyeung1141998@gmail.com>
2021-03-21 22:36:05 -0400

    Fixed navigation swipe bug with title, and created base network manager

--- GitHubFollowers/Screens/FollowerListVC.swift
+++ GitHubFollowers/Screens/FollowerListVC.swift
@@ -13,9 +13,16 @@ class FollowerListVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
-        navigationController?.isNavigationBarHidden = false
         navigationController?.navigationBar.prefersLargeTitles = true
+        NetworkManager.shared.getFollowers(for: username, page: 1) { (followers, errorMessage) in
+            guard let followers = followers else { self.presentGFAlertOnMainThread(title: "Badd Stuff Happened", message: errorMessage!, buttonTitle: "OK"); return }
+            print("Followers.count = \(followers.count)")
+            print(followers)
+        }
     }
     
-    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: true)
+    }
 }
