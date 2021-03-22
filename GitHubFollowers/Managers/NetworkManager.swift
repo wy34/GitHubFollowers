a76def72d3c977258329c56335e4485faac52a4f
William Yeung <williamyeung1141998@gmail.com>
2021-03-22 16:43:25 -0400

    Downloading and caching follower images

--- GitHubFollowers/Managers/NetworkManager.swift
+++ GitHubFollowers/Managers/NetworkManager.swift
@@ -5,11 +5,12 @@
 //  Created by William Yeung on 3/21/21.
 //
 
-import Foundation
+import UIKit
 
 class NetworkManager {
     static let shared = NetworkManager()
-    let baseURL = "https://api.github.com/users/"
+    private let baseURL = "https://api.github.com/users/"
+    let cache = NSCache<NSString, UIImage>()
     
     private init() { }
     
