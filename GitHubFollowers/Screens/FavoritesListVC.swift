49b5e69843c8a3e7aab34a16b1c9b4e7bfcbbb84
William Yeung <williamyeung1141998@gmail.com>
2021-03-24 00:57:14 -0400

    Added base table view for displaying favorites

--- GitHubFollowers/Screens/FavoritesListVC.swift
+++ GitHubFollowers/Screens/FavoritesListVC.swift
@@ -8,21 +8,53 @@
 import UIKit
 
 class FavoritesListVC: UIViewController {
+    
+    let tableView = UITableView()
+    var favorites = [Follower]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureViewController()
+        getFavorites()
+        configureTableView()
+    }
+    
+    func configureViewController() {
+        navigationItem.title = "Favorites"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        view.backgroundColor = .systemBackground
+    }
 
-        view.backgroundColor = .systemBlue
-        
+    func getFavorites() {
         PersistanceManager.retrieveFavorites { (result) in
             switch result {
-            case .success(let favorites):
-                print(favorites)
-            case .failure(let error):
-                break
+                case .success(let favorites):
+                    self.favorites = favorites
+                case .failure(let error):
+                    break
             }
         }
     }
+    
+    func configureTableView() {
+        view.addSubview(tableView)
+        tableView.frame = view.bounds
+        tableView.register(FavoriteCell.self, forCellReuseIdentifier: FavoriteCell.reuseID)
+        tableView.rowHeight = 80
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+}
 
-
+extension FavoritesListVC: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return favorites.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: FavoriteCell.reuseID, for: indexPath) as! FavoriteCell
+        let favorite = favorites[indexPath.row]
+        cell.set(favorite: favorite)
+        return cell
+    }
 }
